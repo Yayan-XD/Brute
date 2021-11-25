d1a2917ebc429925dd260dae34f9539a28eaf76d
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-11-25 16:14:45 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 import sys, os, subprocess
 
-if sys.version[0:3] != "3.9":
-  sys.exit("[+] anda harus menggunakan versi python 3.9, versi python anda sekarang : "+sys.version[0:3])
-
 if __name__ == "__main__":
   try:
       os.system("git pull")
