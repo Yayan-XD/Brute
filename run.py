619919e5a85cffc044d33b319dfc4bc9e19077e4
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-11-25 15:39:38 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if sys.version[0:3] != "3.9":
 if __name__ == "__main__":
   try:
       os.system("git pull")
-      __import__("brute").cok()
+      __import__("brute").moch_yayan()
   except Exception:
     os.system("clear")
     print("\n hello! pengguna script Brute")
