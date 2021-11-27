9fa6aa075ff98ecae552b2822ebbe7d608c0ad46
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-11-27 10:20:48 +0700

    Update run.py

--- run.py
+++ run.py
@@ -4,10 +4,6 @@ if __name__ == "__main__":
   try:
       os.system("git pull")
       __import__("brute").moch_yayan()
-  except Exception:
+  except Exception as e:
     os.system("clear")
-    print("\n hello! pengguna script Brute")
-    print(" script ini telah di perbarui pada tanggal: 13 Juli 2021\n")
-    print(" Tekan enter untuk melihat turtorial cara menjalankan script Brute")
-    input(" enter")
-    exit(subprocess.Popen(["am","start","https://github.com/Yayan-XD/Brute"],stderr=subprocess.PIPE,stdin=subprocess.PIPE,stdout=subprocess.PIPE).wait())
+    print(e)
