2170533314007084be9e83510404d1836fbdc4a4
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-02-07 20:30:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
-import os
+#Hallo anak ngentod :v
 
-if __name__ == "__main__":
-   try:
-       os.system("git pull")
-       __import__("brute").moch_yayan()
-   except Exception as e:
-       exit(str(e))
+import sys, os
+
+N = '\x1b[0m'    # WARNA MATI
+B = '\x1b[1;94m' # BIRU
+M = '\x1b[1;91m' # MERAH
+
+if __name__ == '__main__':
+    os.system("git pull")
+    try:
+        arg_command = sys.argv[1]
+    except IndexError:
+        arg_command = ""
+    if arg_command == 'id':
+    	os.system("python language/id/run.py")
+    elif arg_command == 'en':
+    	os.system("python language/us/run.py")
+    else:
+        exit(f"[{M}!{N}] Jika ingin menggunakan versi bahasa Indonesia\n{N}[{B}>{N}] Ketik python run.py id\n\n[{M}!{N}] If you want to use the English version\n{N}[{B}>{N}] Type python run.py en")
