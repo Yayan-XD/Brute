cc92a2699c75b840b312edbfd756999da58ace4f
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-03-22 18:02:10 +0700

    Update run.py

--- language/id/run.py
+++ language/id/run.py
@@ -1,10 +1,7 @@
 #Hallo anak anjing:v
 
-import os
-
 if __name__ == "__main__":
    try:
-       os.system("git pull")
        __import__("id").moch_yayan()
    except Exception as e:
        exit(str(e))
