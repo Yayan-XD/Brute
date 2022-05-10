e13c798007612737a174d2f0e59d9ad5d603d50e
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-05-10 14:56:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ from src import cok as yy
 
 if __name__ == "__main__":
     os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
-    yy.cek_server()
+    yy.moch_yayan()
