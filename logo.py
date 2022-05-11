b2fc6b26ff8b1573c492b5dd4b5c0f269bb37758
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-05-12 00:01:38 +0700

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,41 @@
+#######################################################
+# Name           : Yayan Multi Brute Facebook (YMBF)  #
+# File           : logo.py                            #
+# Author         : Moch Yayan Juan Alvredo XD.        #
+# Github         : https://github.com/Yayan-XD        #
+# Facebook       : https://www.facebook.com/KM39453   #
+# Website        : https://www.yayanxd.my.id          #
+# Python version : 0.4                                #
+#######################################################
+
+############# DON'T REMOVE THIS FUNCTIONS #############
+
+import sys, os, random
+
+#---- MODULE RICH IN PYTHON -------
+from rich import print as prints
+from rich.panel import Panel
+biru_m = '[bold cyan]'
+hapus  = '[/]'
+
+
+class Logo:
+
+    def __init__(self):
+        if "linux" in sys.platform.lower():
+            try:os.system("clear")
+            except:pass
+        elif "win" in sys.platform.lower():
+            try:os.system("cls")
+            except:pass
+        else:
+            try:os.system("clear")
+            except:pass
+
+    def log(self):
+        WAR = random.choice(["[deep_pink3]","[green]","[cyan]","[blue]"])
+        prints(Panel(f"""{WAR}  ▄▄▄▄· ▄▄▄  ▄• ▄▌▄▄▄▄▄▄▄▄ .    ·▄▄▄▄▄▄▄· 
+  ▐█ ▀█▪▀▄ █·█▪██▌•██  ▀▄.▀·    ▐▄▄·▐█ ▀█▪
+  ▐█▀▀█▄▐▀▀▄ █▌▐█▌ ▐█.▪▐▀▀▪▄    ██▪ ▐█▀▀█▄
+  ██▄▪▐█▐█•█▌▐█▄█▌ ▐█▌·▐█▄▄▌    ██▌.██▄▪▐█
+  ·▀▀▀▀ .▀  ▀ ▀▀▀  ▀▀▀  ▀▀▀     ▀▀▀ ·▀▀▀▀  {WAR}version: 0.4[/]"""))
