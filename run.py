56d3e9e144fcd528d9326a1217bc0cb1ffb7c481
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-05-12 00:10:35 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,18 +1,38 @@
 #######################################################
 # Name           : Yayan Multi Brute Facebook (YMBF)  #
-# File           : run.py                             #
+# File           : run.py                            #
 # Author         : Moch Yayan Juan Alvredo XD.        #
 # Github         : https://github.com/Yayan-XD        #
 # Facebook       : https://www.facebook.com/KM39453   #
 # Website        : https://www.yayanxd.my.id          #
-# Python version : 0.3                                #
+# Python version : 0.4                                #
 #######################################################
 
 ############# DON'T REMOVE THIS FUNCTIONS #############
 
-import os
-from src import cok as yy
 
-if __name__ == "__main__":
+import os
+try:
+    import requests
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
+    os.system('pip install requests')
+
+try:
+    import bs4
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n')
+    os.system('pip install bs4')
+
+try:
+    import rich
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
+    os.system('pip install rich')
+#################################################################################
+import os
+from src import cok
+
+if __name__ == '__main__':
     os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
-    yy.moch_yayan()
+    cok.Brute().moch_yayan()
