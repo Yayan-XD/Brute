02b87574e209403d77d80e3317e889ba5c2f52f5
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-09-18 20:57:29 +0700

    Update run.py

--- run.py
+++ run.py
@@ -34,9 +34,9 @@ from src.cok import Brute
 
 if __name__ == '__main__':
     os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
-    os.system("clear")
-    print("pelase wait....")
-    print()
-    print("script sedang dalam perbaikan..")
-    exit()
-#    Brute()
+   # os.system("clear")
+   # print("please wait....")
+   # print()
+   # print("script sedang dalam perbaikan..")
+   # exit()
+    Brute()
