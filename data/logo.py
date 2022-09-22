713a3592a0d3b8efc5cf34bc7f08a828b978c440
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-09-22 18:00:47 +0700

    Add files via upload

--- data/logo.py
+++ data/logo.py
@@ -10,12 +10,13 @@
 
 ############# DON'T REMOVE THIS FUNCTIONS #############
 
-
-import sys, os
+import sys, os, datetime
 
 #---- MODULE RICH IN PYTHON -------
 from rich import print as prints
+from datetime import datetime
 from rich.panel import Panel
+
 reed = "[bold red]"
 blue = "[bold blue]"
 
@@ -31,13 +32,18 @@ class Logo:
         else:
             try:os.system("clear")
             except:pass
-        self.log()
-
-    def log(self):
         prints(Panel(f"""{blue} ____             _         ______                  {reed} ______ ____  
 {blue}|  _ \           | |       |  ____|                 {reed}|  ____|  _ \ 
 {blue}| |_) |_ __ _   _| |_ ___  | |__ ___  _ __ ___ ___  {reed}| |__  | |_) |
 {blue}|  _ <| '__| | | | __/ _ \ |  __/ _ \| '__/ __/ _ \ {reed}|  __| |  _ < 
 {blue}| |_) | |  | |_| | ||  __/ | | | (_) | | | (_|  __/ {reed}| |    | |_) |
 {blue}|____/|_|   \__,_|\__\___| |_|  \___/|_|  \___\___| {reed}|_|    |____/ 
-                        {reed}BY Yayan XD. @2022""", style="bold white", width=70))
+                        {reed}BY Yayan XD. @2022""", style="bold white", width=70, title=f"[cyan]{self.waktu()}"))
+    def waktu(self):
+        now = datetime.now()
+        hours = now.hour
+        if 4 <= hours < 12:timenow = "good morning"
+        elif 12 <= hours < 15:timenow = "good afternoog"
+        elif 15 <= hours < 18:timenow = "good evening"
+        else:timenow = "good night"
+        return timenow
