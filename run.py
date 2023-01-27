ccf673a7d60636d884471e6d795ffdd5ae592a43
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2023-01-27 14:33:59 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,42 +1,4 @@
-#######################################################
-# Name           : Brute Facebook (BF)                #
-# File           : run.py                             #
-# Author         : Moch Yayan Juan Alvredo XD.        #
-# Github         : https://github.com/Yayan-XD        #
-# Facebook       : https://www.facebook.com/KM39453   #
-# Website        : https://www.yayanxd.my.id          #
-# Python version : 3.11                               #
-#######################################################
-
-############# DON'T REMOVE THIS FUNCTIONS #############
-
-
 import os
-try:
-    import requests
-except ImportError:
-    print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
-    os.system('pip install requests')
-
-try:
-    import bs4
-except ImportError:
-    print('\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n')
-    os.system('pip install bs4')
-
-try:
-    import rich
-except ImportError:
-    print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
-    os.system('pip install rich')
-#################################################################################
-from src.cok import Brute
-
-if __name__ == '__main__':
-    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
-    os.system("clear")
-    print("closed..")
-   # print()
-   # print("script sedang dalam perbaikan..")
-    exit()
-   # Brute()
+os.system("git pull")
+import bhack as e
+e()
