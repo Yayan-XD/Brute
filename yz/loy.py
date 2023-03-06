3e8820dae4295674be1e83d99df389cb4d8f84f5
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2023-03-07 03:23:40 +0700

    Add files via upload

--- yz/loy.py
+++ yz/loy.py
@@ -86,7 +86,7 @@ class Cek_Crack:
                     titid  = kontol.replace(" [✓] ","  \x1b[0m[\x1b[1;92m✓\x1b[0m]\x1b[1;92m ")
                     print(f"{titid}{N}");time.sleep(0.03)
                 prints(Panel(f"     {hijau}PROSES MENGECEK HASIL SELESAI{hapus}", padding=(0,5), style="bold white", width=70))
-                input(f"   [ {O}KEMBALI{N} ] ");sayang.Cindy_aulia("", "")
+                input(f"   [ {O}KEMBALI{N} ] ");sayang.Cindy_aulia()
         elif xz in["2", "02"]:
             try:
                 xxx = os.listdir("results/CP")
@@ -130,7 +130,7 @@ class Cek_Crack:
                     titid  = kontol.replace(" [×] ", "  \x1b[0m[\x1b[1;93m×\x1b[0m]\x1b[1;93m ")
                     print(f"{titid}{N}");time.sleep(0.03)
                 prints(Panel(f"      {kuning}PROSES MENGECEK HASIL SELESAI{hapus}", padding=(0,5), style="bold white", width=70))
-                input(f"   [ {O}KEMBALI{N} ] ");sayang.Cindy_aulia("", "")
+                input(f"   [ {O}KEMBALI{N} ] ");sayang.Cindy_aulia()
         elif xz in["3","03"]:
             prints(Panel(f"""[{biru_m}01{hapus}] hapus hasil ok
 [{biru_m}02{hapus}] hapus hasil cp
@@ -141,18 +141,18 @@ class Cek_Crack:
                 except:os.system("rm -rf results/OK")
                 try:os.mkdir("results/OK")
                 except:pass
-                prints(Panel(f"[{hijau}✓{hapus}] berhasil menghapus semua hasil ok.", style="bold white", width=70));input(f"[ {O}TEKAN ENTER {N} ] ");sayang.Cindy_aulia("", "")
+                prints(Panel(f"[{hijau}✓{hapus}] berhasil menghapus semua hasil ok.", style="bold white", width=70));input(f"[ {O}TEKAN ENTER {N} ] ");sayang.Cindy_aulia()
             elif pil in ["2", "02"]:
                 try:os.remove("results/CP")
                 except:os.system("rm -rf results/CP")
                 try:os.mkdir("results/CP")
                 except:pass
-                prints(Panel(f"[{hijau}✓{hapus}] berhasil menghapus semua hasil cp.", style="bold white", width=70));input(f"[ {O}TEKAN ENTER {N} ] ");sayang.Cindy_aulia("", "")
+                prints(Panel(f"[{hijau}✓{hapus}] berhasil menghapus semua hasil cp.", style="bold white", width=70));input(f"[ {O}TEKAN ENTER {N} ] ");sayang.Cindy_aulia()
             elif pil in ["3", "03"]:
                 self.hasil()
             else:
                 print("");prints(Panel(f"😡 memu [bold red]{pil}[/] tidak ada, cek menu nya!", style="bold white", width=70));time.sleep(3);self.hasil()
         elif xz in["4","04"]:
-            sayang.Cindy_aulia("", "")
+            sayang.Cindy_aulia()
         else:
             print("");prints(Panel(f"😡 memu [bold red]{xz}[/] tidak ada, cek menu nya!", style="bold white", width=70));time.sleep(3);self.hasil()
