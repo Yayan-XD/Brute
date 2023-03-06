8457a27332d5bcb5f407025075a5f5720576fcf0
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2023-03-07 02:12:06 +0700

    Add files via upload

--- data/lainya.py
+++ data/lainya.py
@@ -14,10 +14,9 @@ from rich import print as prints
 from rich.panel import Panel
 
 from yz import cok as dncijjoeppe
-from yxz.bot_fb import Bot_Facebook
 from yz.loy import Cek_Crack as memeo1
 from data.ksabar import Ngocok as xxxxs
-
+from yxz.bot_fb import Bot_Facebook as xx
 
 merah  = '[#FF0022]'
 hijau  = '[#00FF33]'
@@ -41,7 +40,7 @@ class Lain:
         elif pil in["2", "02"]:
             xxxxs()
         elif pil in["3", "03"]:
-            Bot_Facebook(cookie, tokenz)
+            xx(cookie, tokenz)
         elif pil in["0", "00"]:
             dncijjoeppe.Cindy_aulia()
         else:
