24a240614211885f32e818abb1f9e027995ab785
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2023-03-20 12:59:56 +0700

    Update run.py

--- run.py
+++ run.py
@@ -11,35 +11,40 @@
 ############# DON'T REMOVE THIS FUNCTIONS #############
 
 
-import os
+import os, sys
+
+def clear():
+    if "win" in sys.platform:os.system("cls")
+    else:os.system("clear")
+
 try:
     import requests
 except ImportError:
-    print("\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n")
+    clear();print("\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n")
     os.system("pip install requests")
 
 try:
     import bs4
 except ImportError:
-    print("\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n")
+    clear();print("\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n")
     os.system("pip install bs4")
 
 try:
     import Cryptodome
 except ImportError:
-    print("\n [\x1b[1;91m!\x1b[0m] Modul Cryptodome belum terinstall!...\n")
+    clear();print("\n [\x1b[1;91m!\x1b[0m] Modul Cryptodome belum terinstall!...\n")
     os.system("pip install pycryptodomex")
 
 try:
     import nacl.public
 except ImportError:
-    print("\n [\x1b[1;91m!\x1b[0m] Modul PyNaCl belum terinstall!...\n")
+    clear();print("\n [\x1b[1;91m!\x1b[0m] Modul PyNaCl belum terinstall!...\n")
     os.system("pip install PyNaCl")
 
 try:
     import rich
 except ImportError:
-    print("\n [\x1b[1;91m!\x1b[0m] Modul rich belum terinstall!..\n.")
+    clear();print("\n [\x1b[1;91m!\x1b[0m] Modul rich belum terinstall!..\n.")
     os.system("pip install rich")
 ##############################################################################
 from yz.cok import Cindy_aulia as cin
