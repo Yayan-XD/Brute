10bbf815b3c701f554fc6b4f751fe3c81636098a
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2023-03-22 02:02:35 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -33,18 +33,6 @@ except ImportError:
     os.system("pip install bs4")
 
 try:
-    import Cryptodome
-except ImportError:
-    clear();print(f"\n [{M}!{N}] Modul {H}Cryptodome{N} belum terinstall!...\n")
-    os.system("pip install pycryptodomex")
-
-try:
-    import nacl.public
-except ImportError:
-    clear();print(f"\n [{M}!{N}] Modul {H}PyNaCl{N} belum terinstall!...\n")
-    os.system("pip install PyNaCl")
-
-try:
     import rich
 except ImportError:
     clear();print(f"\n [{M}!{N}] Modul {H}Rich{N} belum terinstall!..\n.")
@@ -53,7 +41,7 @@ except ImportError:
 from yz.cok import Cindy_aulia as cin
 
 if __name__=='__main__':
-    try:os.remove("results/OK/...");os.remove("results/CP/...")
-    except:pass
-    os.system("git pull")
+    #try:os.remove("results/OK/...");os.remove("results/CP/...")
+    #except:pass
+    #os.system("git pull")
     cin().moch_yayan()
