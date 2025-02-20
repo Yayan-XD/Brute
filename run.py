4ab3a6259274ddd7dd49bfd3c47ccb9d0f37bc80
Moch Yayan Juan Alvredo XD. <69158355+Yayan-XD@users.noreply.github.com>
2025-02-21 01:22:31 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,27 @@
-import os, sys, subprocess, importlib.util
+import os
+import sys
+import subprocess
+import importlib.util
 
 M = "\x1b[0;31m"  # Merah
 H = "\x1b[0;32m"  # Hijau
 N = "\x1b[0m"     # Reset ke default
+
+class PythonVersionChecker:
+    def __init__(self, required_version):
+        self.required_version = required_version
+        self.current_version = sys.version_info
+
+    def check_version(self):
+        ModuleManager().logo()
+        if self.current_version[:2] == self.required_version:
+            print(f"{H}Notice:{N} Script ini menggunakan Python versi {self.required_version[0]}.{self.required_version[1]}. Versi Python Anda {self.current_version.major}.{self.current_version.minor}.{self.current_version.micro} sudah sesuai.")
+        elif self.current_version[:2] < self.required_version:
+            print(f"{M}Notice:{N} Script ini menggunakan Python versi {self.required_version[0]}.{self.required_version[1]}. Versi Python Anda {self.current_version.major}.{self.current_version.minor}.{self.current_version.micro} terlalu rendah. Silakan upgrade Python Anda.")
+            sys.exit(1)
+        else:
+            print(f"{M}Notice:{N} Script ini menggunakan Python versi {self.required_version[0]}.{self.required_version[1]}. Versi Python Anda {self.current_version.major}.{self.current_version.minor}.{self.current_version.micro} terlalu tinggi. Silakan downgrade Python Anda.")
+            sys.exit(1)
 
 class ModuleManager:
     REQUIRED_MODULES = ["requests", "rich", "bs4"]
@@ -44,7 +63,7 @@ class ModuleManager:
         if "--install" in sys.argv:
             print(f"\n {H}•{N} Menginstal modul yang diperlukan...")
             self.install_modules()
-            print(f"\n {H}✓{N} Semua modul telah diinstal. Silakan jalankan skrip lagi.")
+            print(f" {H}✓{N} Semua modul telah diinstal. Silakan jalankan skrip lagi.")
             sys.exit(0)
 
         if self.missing_modules:
@@ -67,8 +86,9 @@ class ModuleManager:
             else:pass
 
 class Kynara:
-
     def __init__(self):
+        python_checker = PythonVersionChecker((3, 12))
+        python_checker.check_version()
         self.module_manager = ModuleManager()
         self.module_manager.handle_installation()
         self.module_manager.hapus()
