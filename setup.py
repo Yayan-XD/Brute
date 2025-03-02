d97683987fbac3f5f8344d5f465d1a6fad2e0c6e
Moch Yayan Juan Alvredo XD. <69158355+Yayan-XD@users.noreply.github.com>
2025-03-03 06:21:51 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import shutil
 
 FOLDERS = ["bluid", "botfb", "yxdfb", "yxdig"]
 
 extensions = []
 for folder in FOLDERS:
     if not os.path.exists(folder):
-        print(f"Folder {folder} tidak ditemukan. Melewati...")
+        print(f"[!] Folder {folder} tidak ditemukan")
         continue
     for file in os.listdir(folder):
         if file.endswith(".c"):
-            module_name = os.path.splitext(file)[0]
+            module_name = f"{folder}.{file.replace('.c', '')}"
             module_path = os.path.join(folder, file)
-            extensions.append(Extension(f"{folder}.{module_name}", [module_path]))
+            extensions.append(
+                Extension(
+                    module_name,
+                    [module_path],
+                    extra_compile_args=["-O3"]
+                )
+            )
 
-if not extensions:
-    print("Tidak ada file .c yang ditemukan untuk dikompilasi.")
-    sys.exit(1)
+if os.path.exists("build"):
+    shutil.rmtree("build")
 
 setup(
-    ext_modules=cythonize(extensions, language_level=3),
+    ext_modules=cythonize(extensions, language_level=3, build_dir="build"),
     script_args=["build_ext", "--inplace"]
 )
